467d15bf603b9ed9cda6283023e276b8e46fa293
matp33 <Mateusz.pietal@gmail.com>
2016-05-26 13:32:35 +0200

    Removed duplicated directories

--- interfaces/Observable.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-public interface Observable {
-
-	public void addObserver (Observer client);
-	public void removeObserver (Observer client);
-	public void notifyClients ();
-	
-}
